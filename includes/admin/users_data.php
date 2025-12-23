f754ce8f73dad555be71c8c0b7147cbd06fd9b45
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-23 23:06:19 +0100

    feat: update users_data.php to utilize Admin class for user count retrieval

--- includes/admin/users_data.php
+++ includes/admin/users_data.php
@@ -1,12 +1,21 @@
 <?php 
-require_once("../db.php");
+require_once("../../Classes/db.php");
+require_once("../../Classes/User.php");
+require_once("../../Classes/Admin.php");
+
+
+$pdo = new db();
+$admin = new Admin($pdo);
+$all_users = $admin->getAllUsers("all");
+$guides = $admin->getAllUsers("guides_ver");
+
 
 
 if(isset($_POST["users_count"])){
     $type = $_POST["users_count"];
     switch($type){
-        case "all":  echo getUsersCount(""); break;
-        case "notactiveusers": echo getUsersCount("WHERE isActive = 0 AND role = 'guide'");break;
+        case "all":  echo $all_users["count"]; break;
+        case "notactiveusers": echo $guides ["count"];break;
         default:
         echo "0";
         break;
@@ -14,38 +23,13 @@ if(isset($_POST["users_count"])){
     }
 }
 
-if(isset($_POST["users"])){
-    $type = $_POST["users"];
-    switch($type){
-        case "all": echo getUsers("");break;
-        case "pending": echo getUsers("WHERE isActive = 0 AND role = 'guide'");break;
-        default:
-        echo "no one found";
-        break;
-    }
-}
-
-
-
-function getUsersCount($condition = ""){
-    global $conn;
-    $query= "SELECT COUNT(*) as count FROM users $condition";
-    $result = mysqli_query($conn,$query);
-    if($result){
-    $row = mysqli_fetch_assoc($result);
-    return $row["count"];
-    }
-    return 0;
-   
-}
-
-
-function getUsers($user_condition){
-    global $conn;
-    $query = "SELECT * FROM users $user_condition";
-    $result = mysqli_query($conn,$query);
-
-    $row = mysqli_fetch_all($result,MYSQLI_ASSOC);
-
-    return json_encode($row);
-}
+// if(isset($_POST["users"])){
+//     $type = $_POST["users"];
+//     switch($type){
+//         case "all": echo getUsers("");break;
+//         case "pending": echo getUsers("");break;
+//         default:
+//         echo "no one found";
+//         break;
+//     }
+// }
