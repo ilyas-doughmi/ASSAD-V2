29268cc4500b2fbc4cca2c8b951329395bdbee98
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-24 14:40:45 +0100

    refactor: improve db connection handling by implementing singleton pattern

--- Classes/db.php
+++ Classes/db.php
@@ -1,22 +1,34 @@
 <?php
 
-Class db{
+class db
+{
     private $host = "localhost";
     private $username = "root";
     private $password = "";
     private $db_name = "assad";
 
-    public function connect(){
-         try{
-            $pdo = new PDO("mysql:host=".$this->host.";dbname=".$this->db_name,$this->username,$this->password,
-            [
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-            ]);
-            return $pdo;
+    private static $instance = null;
 
-        }catch(PDOException $e){
-            die($e->getMessage());
+    public function connect()
+    {
+        if (self::$instance == null) {
+            try {
+                self::$instance = new PDO(
+                    "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
+                    $this->username,
+                    $this->password,
+                    [
+                        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+                    ]
+                );
+                
+            } catch (PDOException $e) {
+                die($e->getMessage());
+            }
         }
-}
+        else{
+            return self::$instance;
+        }
+    }
 }
