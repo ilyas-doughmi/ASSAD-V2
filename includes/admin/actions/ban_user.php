77560b2d29ccb46646a886ca02904f877a14a0a6
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-24 10:04:11 +0100

    feat: refactor ban_user.php to use Admin class for banning accounts

--- includes/admin/actions/ban_user.php
+++ includes/admin/actions/ban_user.php
@@ -1,19 +1,14 @@
 <?php
-require_once("../../db.php");
+require_once("../../../Classes/db.php");
+require_once("../../../Classes/User.php");
+require_once("../../../Classes/Admin.php");
+
+
+$pdo = new db();
+$admin = new Admin($pdo);
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
-    $user_id = $_POST["user_id"];
-    
-    $query = "UPDATE users SET isBanned = 1 WHERE id = ?";
-
-    $stmt = mysqli_prepare($conn,$query);
-    
-    if ($stmt === false) {
-        die("Error prepare statement" . mysqli_error($conn));
-    }
-
-    mysqli_stmt_bind_param($stmt,"i",$user_id);
-    mysqli_stmt_execute($stmt);
-
-    echo "Active Successfully";
+    $guide_id = $_POST["user_id"];
+    $admin->banAccount($guide_id);
+    echo "ban Successfully";
 }
