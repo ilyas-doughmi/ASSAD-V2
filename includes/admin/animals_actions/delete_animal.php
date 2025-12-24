de0285e3592c634adeea27d37f110a826b3c6999
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-24 12:05:30 +0100

    refactor: simplify deleteAnimal functionality by utilizing Animal class method

--- includes/admin/animals_actions/delete_animal.php
+++ includes/admin/animals_actions/delete_animal.php
@@ -1,23 +1,14 @@
 <?php
-require_once("../../db.php");
+require_once("../../../Classes/db.php");
+require_once("../../../Classes/Animal.php");
+
+
+$pdo = new db();
+$animal = new Animal($pdo);
+
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $id = $_POST["id"];
-
-    $query = "DELETE FROM animal WHERE id = ?";
-    $stmt = mysqli_prepare($conn, $query);
-
-    if ($stmt === false) {
-        die("Error prepare statement" . mysqli_error($conn));
-    }
-
-    mysqli_stmt_bind_param($stmt, "i", $id);
-
-    try {
-        mysqli_stmt_execute($stmt);
-        echo "success";
-    } catch (mysqli_sql_exception $e) {
-        echo "error: " . $e->getMessage();
-    }
+    $animal->deleteAnimal($id);
 }
 ?>
