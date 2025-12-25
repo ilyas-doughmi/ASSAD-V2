75da9210fe593d25c0d0b96a1db654d071ab89fd
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-25 18:47:49 +0100

    feat: remove guide management methods from Admin class to streamline functionality

--- Classes/Admin.php
+++ Classes/Admin.php
@@ -2,24 +2,4 @@
 
 Class Admin extends User{
 
-
-    public function approveGuide($guide_id){
-        $query = "UPDATE users SET isActive = 1 WHERE id = :guide_id";
-        $stmt = $this->pdo->connect()->prepare($query);
-        $stmt->bindParam(":guide_id",$guide_id);
-        $stmt->execute();
-    }
-
-    public function rejectGuide($guide_id){
-        $query = "UPDATE users SET isBanned = 1 , isActive = 1 WHERE id = :guide_id";
-        $stmt = $this->pdo->connect()->prepare($query);
-        $stmt->bindParam(":guide_id",$guide_id);
-        $stmt->execute();
-    }
-    public function banAccount($guide_id){
-        $query = "UPDATE users SET isBanned = 1 , isActive = 1 WHERE id = :guide_id";
-        $stmt = $this->pdo->connect()->prepare($query);
-        $stmt->bindParam(":guide_id",$guide_id);
-        $stmt->execute();
-    }
 }
