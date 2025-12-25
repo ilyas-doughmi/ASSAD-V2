16fd526c0c01a1421ade8b9fedce2560a06db0fe
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-25 11:30:49 +0100

    feat: refactor gethabitatName.php to utilize Habitat class for habitat retrieval

--- includes/admin/habitat_actions/gethabitatName.php
+++ includes/admin/habitat_actions/gethabitatName.php
@@ -1,30 +1,14 @@
 <?php 
 
-require_once("../../db.php");
+require_once("../../../Classes/db.php");
+require_once("../../../Classes/Habitat.php");
 
+$pdo = new db();
+$habitat = new Habitat($pdo);
 
 if(isset($_POST["habitat_id"])){
     $habitat_id = $_POST["habitat_id"];
-    echo habitat_name($habitat_id);
+    $habitat->getHabitatById($habitat_id);  
 }
 
 
-function habitat_name($id){
-    global $conn;
-    $query = "SELECT * FROM habitat WHERE id = ?";
-    $stmt = mysqli_prepare($conn,$query);
-
-    if ($stmt === false) {
-        die("Error prepare statement" . mysqli_error($conn));
-    }
-
-    mysqli_stmt_bind_param($stmt,"i",$id);
-    
-    mysqli_stmt_execute($stmt);
-
-    $result = mysqli_stmt_get_result($stmt);
-
-    $data = mysqli_fetch_assoc($result);
-
-    return $data["nom"];
-}
