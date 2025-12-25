10f102162e69256f862805197f82481b196b3dc0
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-25 17:03:08 +0100

    feat:  guide class to extend User and implement active status management

--- Classes/guide.php
+++ Classes/guide.php
@@ -1,12 +1,38 @@
 <?php 
 
-class guide{
+class guide extends User{
     protected $isActive;
 
+    
 
-    public function setIsActive(int $isActive): void
+    public function setIsActive(int $guide_id): string
     {
-        $this->isActive = $isActive;
+        $query = "UPDATE users SET isActive = 1 WHERE id = :guide_id";
+        $stmt = $this->pdo->connect()->prepare($query);
+        $stmt->bindParam(":guide_id",$guide_id);
+
+        try{
+             $stmt->execute();
+             return "approved";
+        }catch(PDOException $e){
+            return $e->getMessage();
+        }
+       
     }
+
+    public function getGuides()
+    {
+        $query = "SELECT * FROM users WHERE role = 'guide' AND isActive = 0";
+        $stmt = $this->pdo->connect()->prepare($query);
+        $stmt->execute();
+        $guides = $stmt->fetchAll();
+        $count = count($guides);
+
+        return [
+            'count' => $count,
+            'users' => $guides
+        ];
+    }
+
 
 }
