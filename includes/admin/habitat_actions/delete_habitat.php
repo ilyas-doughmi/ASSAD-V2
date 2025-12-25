c979c12074496f7e4f6af9440b1c34d53daa0d0d
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-25 12:13:12 +0100

    feat: streamline habitat deletion process by utilizing Habitat class method

--- includes/admin/habitat_actions/delete_habitat.php
+++ includes/admin/habitat_actions/delete_habitat.php
@@ -1,26 +1,14 @@
 <?php
-require_once("../../db.php");
+
+require_once("../../../Classes/db.php");
+require_once("../../../Classes/Habitat.php");
+
+$pdo = new db();
+$habitat = new Habitat($pdo);
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $id = $_POST["id"];
 
-    $query = "DELETE FROM habitat WHERE id = ?";
-    $stmt = mysqli_prepare($conn, $query);
-
-    if ($stmt === false) {
-        die("Error prepare statement" . mysqli_error($conn));
-    }
-
-    mysqli_stmt_bind_param($stmt, "i", $id);
-
-    try {
-        if(mysqli_stmt_execute($stmt)){
-            echo "success";
-        } else {
-            echo "error_exec";
-        }
-    } catch (mysqli_sql_exception $e) {
-        echo "error: " . $e->getMessage();
-    }
+    $habitat->deleteHabitat($id);
 }
 ?>
