418dfeb67a27b73a72bfa57109ebcecfceeb1aa3
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-25 11:53:18 +0100

    refactor: streamline animal creation process by utilizing setter methods

--- includes/admin/animals_actions/create_animal.php
+++ includes/admin/animals_actions/create_animal.php
@@ -1,22 +1,20 @@
 <?php
+
 require_once("../../../Classes/db.php");
 require_once("../../../Classes/Animal.php");
-
 
 $pdo = new db();
 $animal = new Animal($pdo);
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    
-    $nom = $_POST["nom"];
-    $espece = $_POST["espece"];
-    $pays = $_POST["pays"];
-    $habitat_id = $_POST["habitat_id"]; 
-    $description = $_POST["description"];
-    $alimentation = $_POST["alimentation"];
-    $image = $_POST["image"];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-    $animal->createAnimal($nom,$espece,$pays,$habitat_id,$description,$alimentation,$image);
+    $animal->setNom($_POST['nom']);
+    $animal->setEspece($_POST['espece']);
+    $animal->setPaysOrigin($_POST['pays']);
+    $animal->setHabitatId((int) $_POST['habitat_id']);
+    $animal->setDescription($_POST['description']);
+    $animal->setAlimentation($_POST['alimentation']);
+    $animal->setImage($_POST['image']);
 
+    $animal->createAnimal();
 }
-?>
