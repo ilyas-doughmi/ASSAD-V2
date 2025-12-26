0d69d63ed0cc083f66a38ad1f28626efb4b69c85
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-26 12:58:12 +0100

    feat: refactor reserve_tour.php to use PDO and Reservation class for creating reservations

--- includes/guide/visite_action/reserve_tour.php
+++ includes/guide/visite_action/reserve_tour.php
@@ -1,31 +1,25 @@
 <?php
 session_start();
 
-require_once("../../db.php");
+require_once("../../../Classes/db.php");
+require_once("../../../Classes/Reservation.php");
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     $tour_id = $_POST["reserve"];
     $user_id = $_SESSION["id"];
-    $query = "INSERT INTO reservation(user_id,tour_id)
-            VALUES(?,?)";
 
-    $stmt = mysqli_prepare($conn,$query);
+    $db = new db();
+    $pdo = $db->connect();
 
-    if($stmt == false){
-        die("die");
-    }
-
-    mysqli_stmt_bind_param($stmt,"ii",$user_id,$tour_id);
+    $reservation = new reservation($pdo);
+    $reservation->setUserId($user_id);
+    $reservation->setTourId($tour_id);
 
     try{
-        mysqli_stmt_execute($stmt);
+        $reservation->createReservation();
         echo "reservation success";
 
-    }catch(mysqli_sql_exception $e){
+    }catch(Exception $e){
         die($tour_id);
     }
-
-
-
-    // $result = mysqli_stmt_get_result($stmt);
 }
