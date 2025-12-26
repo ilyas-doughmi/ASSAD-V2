61b59a982c6b9ddfb90a81c847886e2d22021f57
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-26 09:37:34 +0100

    feat: initialize PDO in tour_step class constructor and implement addStep method

--- Classes/tour_step.php
+++ Classes/tour_step.php
@@ -2,11 +2,17 @@
 
 class tour_step
 {
+    private $pdo;
     private $id;
     private $titre_etape;
     private $description_etape;
     private $order_etape;
     private $tour_id;
+
+    public function __construct($pdo)
+    {
+        $this->pdo = $pdo;
+    }
 
     public function getId()
     {
@@ -60,7 +66,16 @@ class tour_step
 
 
     public function addStep(){
-
+        $query = "INSERT INTO tour_step(titre_etape,description_etape,order_etape,tour_id)
+        VALUES(:titre, :description, :order, :tour_id)";
+        $stmt = $this->pdo->prepare($query);
+        
+        $stmt->execute([
+            ':titre' => $this->titre_etape,
+            ':description' => $this->description_etape,
+            ':order' => $this->order_etape,
+            ':tour_id' => $this->tour_id
+        ]);
     }
 
     public function editStep(){
