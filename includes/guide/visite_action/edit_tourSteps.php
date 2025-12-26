890ad9c7cf3cd095327f9ccf69331dd1d330cdde
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-26 12:42:53 +0100

    feat: add edit_tourSteps.php for handling tour step edits

---
+++ includes/guide/visite_action/edit_tourSteps.php
@@ -0,0 +1,29 @@
+<?php
+require_once("../../../Classes/db.php");
+require_once("../../../Classes/tour_step.php");
+
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $id = $_POST["step_id"];
+    $title = $_POST["step_title"];
+    $description = $_POST["step_description"];
+    $etape = $_POST["step_order"];
+    $tour_id = $_POST["tour_id"];
+
+    $db = new db();
+    $pdo = $db->connect();
+
+    $tourStep = new tour_step($pdo);
+    $tourStep->setTitreEtape($title);
+    $tourStep->setDescriptionEtape($description);
+    $tourStep->setOrderEtape($etape);
+    $tourStep->setTourId($tour_id);
+
+    try{
+        $tourStep->editStep($id);
+        echo "done";
+    }catch(Exception $e){
+        echo "problem";
+    }
+    
+}
