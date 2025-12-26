782a330a80f322846349537a62694a250ec83fb6
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-26 12:31:39 +0100

    feat: refactor visites_data.php to utilize Tour class for fetching tours and remove legacy function

--- includes/visitor/visites_data.php
+++ includes/visitor/visites_data.php
@@ -1,14 +1,12 @@
 <?php
-require_once __DIR__ . "/../db.php";
+require_once("../../Classes/db.php");
+require_once("../../Claases/Tour.php");
+
+$pdo = new db();
+$tour =  new Tour($pdo);
 
 if(isset($_POST["gettours"])){
-    echo getAllTours();
+    echo json_encode($tour->getAllTour());
 }
 
-function getAllTours(){
-    global $conn;
-    $query = "SELECT * FROM tours WHERE status = 'open' AND date_heure_debut >= NOW() ORDER BY date_heure_debut ASC";
-    $result = mysqli_query($conn, $query);
-    return json_encode(mysqli_fetch_all($result, MYSQLI_ASSOC));
-}
 ?>
