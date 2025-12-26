a5b65b3abf901517dcd0a9fe5723580f6d7d364e
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-26 08:58:10 +0100

    feat: extend Visitor class from User and implement ban/unban account methods

--- Classes/Visitor.php
+++ Classes/Visitor.php
@@ -1,5 +1,24 @@
 <?php
 
-class visitor{
+class visitor extends User{
     protected $isBanned;
+
+
+    public function banAccount(int $user_id): void
+    {
+        $query = "UPDATE users SET isBanned = 1 , isActive = 1 WHERE id = :user_id";
+        $stmt = $this->pdo->connect()->prepare($query);
+        $stmt->bindParam(":user_id", $user_id);
+        $stmt->execute();
+    }
+
+    public function unBanAccount(int $user_id): void
+    {
+        $query = "UPDATE users SET isBanned = 0 , isActive = 1 WHERE id = :user_id";
+        $stmt = $this->pdo->connect()->prepare($query);
+        $stmt->bindParam(":user_id", $user_id);
+        $stmt->execute();
+    }
+
+
 }
