98897871346d89f7b760971fc86a68dd1d882072
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-27 11:38:29 +0100

    refactor: updating tour reservation counting to use reservation class

--- includes/guide/get_tour_reservations.php
+++ includes/guide/get_tour_reservations.php
@@ -1,16 +1,12 @@
 <?php
-require_once("../../includes/db.php");
+require_once("../../Classes/db.php");
+require_once("../../Classes/Reservation.php");
 
 $tour_id = $_GET['tour_id'];
 
-$query = "SELECT COUNT(*) as count FROM reservation WHERE tour_id = ?";
+$pdo = new db();
+$db = $pdo->connect();
 
-$stmt = mysqli_prepare($conn, $query);
-mysqli_stmt_bind_param($stmt, "i", $tour_id);
-
-mysqli_stmt_execute($stmt);
-
-$result = mysqli_stmt_get_result($stmt);
-$row = mysqli_fetch_assoc($result);
-echo $row['count'];
+$reservation = new reservation($db);
+echo $reservation->countReservationsByTour($tour_id);
 ?>
