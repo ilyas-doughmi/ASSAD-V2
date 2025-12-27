be21645ddb5256061ecb2219c8e5146f0b9321b3
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-27 11:38:06 +0100

    refactor: updating edit visite to use tour class methods

--- includes/guide/visite_action/edit_visite.php
+++ includes/guide/visite_action/edit_visite.php
@@ -1,43 +1,29 @@
 <?php
-require_once("../../db.php");
+require_once("../../../Classes/db.php");
+require_once("../../../Classes/Tour.php");
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
-    $id = $_POST['id'];
-    $titre = $_POST['titre'];
-    $date = $_POST['date_heure_debut'];
-    $duree = $_POST['duree_minutes'];
-    $prix = $_POST['prix'];
-    $langue = $_POST['langue'];
-    $capacite = $_POST['capacity_max'];
-    $description = $_POST['description'];
-    $image = $_POST['tour_image']; 
+    $pdo = new db();
+    $db = $pdo->connect(); 
 
-    $query = "UPDATE Tours SET 
-              titre = ?, 
-              description = ?, 
-              date_heure_debut = ?, 
-              duree_minutes = ?, 
-              prix = ?, 
-              langue = ?, 
-              capacity_max = ?, 
-              tour_image = ? 
-              WHERE id = ?";
-    
-    $stmt = mysqli_prepare($conn, $query);
+    $tour = new tour($db);
 
-    if ($stmt === false) {
-        die("Erreur de préparation : " . mysqli_error($conn));
-    }
+    $tour->setId($_POST['id']);
+    $tour->setTitle($_POST['titre']);
+    $tour->setDescription($_POST['description']);
+    $tour->setDateHeureDebut($_POST['date_heure_debut']);
+    $tour->setDureeMinutes($_POST['duree_minutes']);
+    $tour->setPrix($_POST['prix']);
+    $tour->setLangue($_POST['langue']);
+    $tour->setCapacityMax($_POST['capacity_max']);
+    $tour->setTourImage($_POST['tour_image']);
 
-    
-    mysqli_stmt_bind_param($stmt, "sssidsssi", $titre, $description, $date, $duree, $prix, $langue, $capacite, $image, $id);
-
-    if (mysqli_stmt_execute($stmt)) {
+    if ($tour->editTour()) {
         header("Location: ../../../pages/guide/guide_tours.php?msg=updated");
         exit();
     } else {
-        echo "Erreur lors de la mise à jour : " . mysqli_error($conn);
+        echo "Erreur lors de la mise à jour.";
     }
 }
 ?>
